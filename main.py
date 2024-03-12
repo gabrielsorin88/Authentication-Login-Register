34ee7127eab92a45a74602d774b0d446726e3e86
Vasile Sorin Gabriel <gabrielsorin88@gmail.com>
2024-03-12 10:18:58 +0200

    Add Hash and Salt

---
+++ main.py
@@ -0,0 +1,78 @@
+from flask import Flask, render_template, request, url_for, redirect, flash, send_from_directory
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String
+from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret-key-goes-here'
+
+# CREATE DATABASE
+class Base(DeclarativeBase):
+    pass
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+db = SQLAlchemy(model_class=Base)
+db.init_app(app)
+
+# CREATE TABLE IN DB
+class User(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    email: Mapped[str] = mapped_column(String(100), unique=True)
+    password: Mapped[str] = mapped_column(String(100))
+    name: Mapped[str] = mapped_column(String(1000))
+ 
+with app.app_context():
+    db.create_all()
+
+#generate_password_hash()
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        #hashing users password
+        plain_text_pw = request.form.get('password')
+        hash_password = generate_password_hash(plain_text_pw, method='pbkdf2:sha256', salt_length=8)
+        new_user = User(
+            email= request.form.get('email'),
+            name=request.form.get('name'),
+            password = hash_password
+        )
+        db.session.add(new_user)
+        db.session.commit()
+        user_name= request.form.get('name')
+
+        return render_template('secrets.html', user_name=user_name )
+    return render_template("register.html")
+
+
+@app.route('/login')
+def login():
+    return render_template("login.html")
+
+
+@app.route('/secrets')
+def secrets():
+    return render_template("secrets.html")
+
+
+@app.route('/logout')
+def logout():
+    pass
+
+
+@app.route('/download')
+def download():
+    return send_from_directory(directory='static', path="files/cheat_sheet.pdf" )
+    #the directory should be relative to the rooth path
+    #the path should be relative to the directory
+    #if you want to just downloadit as an attachment tou can use the arg: as_attachment=True,  after the path
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
